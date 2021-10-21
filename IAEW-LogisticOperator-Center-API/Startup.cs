87e6fdd82ea1794c1c8869d0ba8b44e9190cf412
Rodrigo <rodrigotdmw@gmail.com>
2021-10-21 16:32:45 -0300

    Se corrige el archivo start up para que funcione

--- IAEW-LogisticOperator-Center-API/Startup.cs
+++ IAEW-LogisticOperator-Center-API/Startup.cs
@@ -17,15 +17,17 @@ namespace IAEW_LogisticOperator_Center_API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             services.AddSwaggerGen(options =>
             {
                 var groupName = "v1";
 
                 options.SwaggerDoc(groupName, new OpenApiInfo
                 {
-                    Title = $"API Operador logÌstico centro {groupName}",
+                    Title = $"API Operador log√≠stico centro {groupName}",
                     Version = groupName,
-                    Description = "DocumentaciÛn de la API del operador logÌstico centro, para el trabajo pr·ctico integrador de la materia 'IntegraciÛn de Aplicaciones Web', UTN FRC 2021",
+                    Description = "Documentaci√≥n de la API del operador log√≠stico centro, para el trabajo pr√°ctico integrador de la materia 'Integraci√≥n de Aplicaciones Web', UTN FRC 2021",
                     Contact = new OpenApiContact
                     {
                         Name = "Foo Company",
