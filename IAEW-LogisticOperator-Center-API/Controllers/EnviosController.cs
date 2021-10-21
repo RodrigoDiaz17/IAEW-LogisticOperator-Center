08e1d1d0525b98efdc31355ac04f04c4ee6f4ef0
Isaias Avalle <isaiasavalle9@hotmail.com>
2021-10-21 17:36:22 -0300

    Se agregan endpoints para envío

--- IAEW-LogisticOperator-Center-API/Controllers/EnviosController.cs
+++ IAEW-LogisticOperator-Center-API/Controllers/EnviosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IAEW_LogisticOperator_Center_API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,37 @@ namespace IAEW_LogisticOperator_Center_API.Controllers
     [ApiController]
     public class EnviosController : ControllerBase
     {
+
+        [HttpGet("ordenes_envio/{orden_envio}")]
+        public IActionResult GetById(long id)
+        {
+            // getear envio
+            return Ok(new DatosEnvio());
+        }
+
+        [HttpPost("ordenes_envio")]
+        public IActionResult CreateOrder([FromBody] DatosEnvio datosEnvio) 
+        {
+            // crear envio
+            var createdUri = $"uri del envio creado a armar despues";
+            var createdEnvio = new DatosEnvio(); // retornar el envio creado despues
+            return Created(createdUri, createdEnvio);
+        }
+
+        [HttpPost("ordenes_envio/{orden_envio}/repartidor/{id_repartidor}")]
+        public IActionResult AssignDealer(long orden_envio , long id_repartidor)
+        {
+            // asignar repartidor
+            
+            return Ok();
+        }
+
+        [HttpPost("ordenes_envio/{orden_envio}/entrega")]
+        public IActionResult RegisterDelivery([FromRoute] long orden_envio)
+        {
+            // actualizar estado y activar webhook
+
+            return Ok();
+        }
     }
 }
