3d5764cbc929c9f7aceda0f876d607589dc6ad19
Nicolás Sivoff <nicosivoff@gmail.com>
2021-10-21 17:37:28 -0300

    Agrego repartidor controller

---
+++ IAEW-LogisticOperator-Center-API/Controllers/RepartidoresController.cs
@@ -0,0 +1,36 @@
+﻿using IAEW_LogisticOperator_Center_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAEW_LogisticOperator_Center_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepartidoresController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [Route("{repartidorId}")]
+        [HttpGet]
+        public IActionResult GetById(long repartidorId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Repartidor repartidor)
+        {
+            return Ok();
+        }
+
+        [Route("{repartidorId}")]
+        [HttpDelete]
+        public IActionResult Delete(long repartidorId)
+        {
+            return Ok();
+        }
+    }
+}
